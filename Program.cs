3e7f92d0398734555d595e909fa131782f8c6c76
Jokūbas <jokis125@gmail.com>
2020-04-06 00:38:09 +0300

    Fixed WriteLine

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace OS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello OS");
+            Console.WriteLine("Hey guys christian bale here welcome to another tutorial on how to get max points" +
+                              "from OS :)");
         }
     }
 }
