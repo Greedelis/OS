ec1e9ab2e05d1b8d3ce8d3d9e6fa4e9362ab1247
Greedelis <andriusbglns0@gmail.com>
2020-04-05 20:56:56 +0300

    lol

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace OS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello OS");
         }
     }
 }
