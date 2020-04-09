6d13b77041209906e1ffd5ae33d939b9cada2e0b
Greedelis <andriusbglns0@gmail.com>
2020-04-09 16:50:07 +0300

    added word class

--- Memory.cs
+++ Memory.cs
@@ -1,6 +1,52 @@
 ﻿using System;
 
 namespace OS {
+        class word{
+        private byte[] wordd = new byte[4]{0, 0, 0, 0};
+
+        public UInt32 toInt32(){
+            return (UInt32)Math.Pow(16,6) * wordd[0] + (UInt32)Math.Pow(16,4) * wordd[1] + (UInt32)Math.Pow(16,2) * wordd[2] + wordd[3];
+        }
+        public String toString(){
+            char[] temp_arr = {(char)wordd[0],(char)wordd[1],(char)wordd[2],(char)wordd[3]};
+            return new string(temp_arr);
+        }
+        public void setValue(UInt32 newVal){
+            wordd[0] = (byte)(newVal/((UInt32)Math.Pow(16,6)));
+            newVal -= wordd[0] * (UInt32)Math.Pow(16,6);
+            wordd[1] = (byte)(newVal/((UInt32)Math.Pow(16,4)));
+            newVal -= wordd[1] * (UInt32)Math.Pow(16,4);
+            wordd[2] = (byte)(newVal/((UInt32)Math.Pow(16,2)));
+            wordd[3] = (byte)(newVal - wordd[2] * (UInt32)Math.Pow(16,2));
+        }
+        public void setValue(string newVal){
+            int leng = newVal.Length;
+            if(leng >= 4){
+                for(int i = 0; i < 4; i++){
+                    wordd[i] = (byte)newVal[i];
+                }
+            }else if(leng < 4){
+                for(int i = 0; i < leng; i++){
+                    wordd[3-i] =  (byte)newVal[i];
+                }
+                for(int y = leng-1; y < 4; y++){
+                    wordd[y]=0;
+                }
+            }
+        }
+        public void printValues(){
+            foreach (var item in wordd){
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+        }
+        public void printCharValues(){
+            foreach (var item in wordd){
+                Console.Write("{0} ", (char)item);
+            }
+            Console.WriteLine();
+        }
+    }
     public class Memory {
 
         private int[] m_data = new int[16 * 16 * 80]; // I don't fucking know
